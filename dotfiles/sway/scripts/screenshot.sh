b3f27ce60081140175de695a197c6b46b354b300
labi-le <1a6i1e@gmail.com>
2022-02-08 19:41:48 +0300

    add intelij

--- dotfiles/sway/scripts/screenshot.sh
+++ dotfiles/sway/scripts/screenshot.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
  
-entries="Active Screen Output Area Window"
+entries="Area Window"
  
 selected=$(printf '%s\n' $entries | wofi --style=$HOME/.config/wofi/style.widgets.css --conf=$HOME/.config/wofi/config.screenshot | awk '{print tolower($1)}')
  
 case $selected in
-  active)
-    /usr/share/sway/scripts/grimshot --notify save active;;
-  screen)
-    /usr/share/sway/scripts/grimshot --notify save screen;;
-  output)
-    /usr/share/sway/scripts/grimshot --notify save output;;
   area)
-    /usr/share/sway/scripts/grimshot --notify save area;;
+    /usr/share/sway/scripts/grimshot copy area;;
   window)
-    /usr/share/sway/scripts/grimshot --notify save window;;
+    /usr/share/sway/scripts/grimshot copy window;;
 esac
